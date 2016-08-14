33715747554f64745c4d1a15e247d223c57d9997
Vladimir Li <vladde1991@gmail.com>
2016-08-14 16:42:24 +0200

    publishing Twist msg with deltas and tag_orientation

--- src/pose_estimation.py
+++ src/pose_estimation.py
@@ -2,6 +2,7 @@
 import rospy
 from ardrone_autonomy.msg import Navdata
 from sensor_msgs.msg import CameraInfo
+from geometry_msgs.msg import Twist
 import numpy as np
 
 
@@ -11,7 +12,7 @@ acc = [0,0,0]
 acc_bias = [-0.0905859717205,   0.0330284067355,   0.0430109761953]
 thetas = [0.0,0.0]
 def callback(data):
-    global acc
+    global acc, pub
     #print "number of tags",data.tags_count
     acc[0] = data.ax+acc_bias[0]
     acc[1] = data.ay+acc_bias[1]
@@ -21,27 +22,36 @@ def callback(data):
     thetas[1] = np.arctan(acc[1]/acc[2])
 
     if data.tags_count > 0:
-        print
         for i in range(data.tags_count):
             if data.tags_type > 100000:
-                estimate_X_and_Y(data.tags_xc[i],data.tags_yc[i],data.tags_distance[i])
-                # x_displacement = data.tags_xc[i] - 500
-                # y_displacement = data.tags_yc[i] - 500
-                # print "tag_x", x_displacement
-                # print "tag_y", y_displacement
-                # print "tags_distance", data.tags_distance[i]
-                # tag_orientation = (data.tags_orientation[i] + 90.0)
-                # if tag_orientation >= 360:
-                #     tag_orientation -= 360
-                # print "tags_orientation", tag_orientation
+                [alphaX,alphaY] = estimate_alphaX_and_alphaY(data.tags_xc[i],data.tags_yc[i],data.tags_distance[i])
+                cZ =  data.tags_distance[i]
+                dX = cZ*np.sin(thetas[1]-alphaX)
+                dY = -cZ*np.sin(thetas[0]-alphaY)
 
-def estimate_X_and_Y(xc,yc,zc):
+                tag_orientation = (data.tags_orientation[i] + 90.0)
+                if tag_orientation >= 360:
+                    tag_orientation -= 360
+                msg = Twist()
+                msg.linear.x = dX
+                msg.linear.y = dY
+                msg.linear.z = 0
+                msg.angular.x = 0
+                msg.angular.y = 0
+                msg.angular.z = tag_orientation
+                pub.publish(msg)
+
+
+def estimate_alphaX_and_alphaY(xc,yc,zc):
     global P, width_and_height
     x = xc*width_and_height[0]/1000
     y = yc*width_and_height[1]/1000
     X = zc*(x-P[2])/P[0]
     Y = zc*(y-P[6])/P[5]
-    print "X: ", X, " Y: ",Y
+    return [np.arctan(X/zc),np.arctan(Y/zc)]
+
+def estimate_roll_pitch(ax,ay,az):
+    return[np.arctan(ax/az),np.arctan(ay/az)]
 
 def camera_info_cb(data):
     global P, width_and_height
@@ -53,9 +63,10 @@ def camera_info_cb(data):
 
 # Intializes everything
 def start():
+    global pub
     # publishing to "turtle1/cmd_vel" to control turtle1
     rospy.init_node('pose_estimation')
-    #pub = rospy.Publisher('', Twist, queue_size=10)
+    pub = rospy.Publisher('tag_delta', Twist, queue_size=1)
     # subscribed to joystick inputs on topic "joy"
     rospy.Subscriber("/ardrone/navdata", Navdata, callback)
     rospy.Subscriber("/ardrone/bottom/camera_info", CameraInfo, camera_info_cb)
